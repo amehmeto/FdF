58b3658fa51f96b2e2200bbf3072c62afeade696
Arthur MEHMETOGLU <amehmeto@tudent.42.fr>
2018-01-05 13:08:06 +0100

    refactorisation main.c

--- src/main.c
+++ src/main.c
@@ -6,13 +6,13 @@
 /*   By: amehmeto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/11/21 00:43:09 by amehmeto          #+#    #+#             */
-/*   Updated: 2018/01/05 11:10:13 by amehmeto         ###   ########.fr       */
+/*   Updated: 2018/01/05 13:07:08 by amehmeto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./fdf.h"
 
-int		my_key_funct(int keycode, void *param)
+int				my_key_funct(int keycode, void *param)
 {
 	(void)param;
 	printf("key event %d\n", keycode);
@@ -21,13 +21,15 @@ int		my_key_funct(int keycode, void *param)
 	return (0);
 }
 
-static void		draw_tile(t_line *l, t_env *e)
+static void		draw_tile(t_line *l, t_env *e, t_coor c, char ***map, int var)
 {
+	margin_adjustment(l, c, var);
+	z_adjustment(l, c, map, var);
 	iso_adjustment(l);
 	draw_line(l, e);
 }
 
-void	draw_wireframe(char ***map, t_env *e)
+void			draw_wireframe(char ***map, t_env *e)
 {
 	t_line	l;
 	t_coor	c;
@@ -40,41 +42,25 @@ void	draw_wireframe(char ***map, t_env *e)
 		while (map[c.y][c.x])
 		{
 			if (map[c.y][c.x + 1])
-			{
-				margin_adjustment(&l, c, 1);
-				z_adjustment(&l, c, map, 1);
-				draw_tile(&l, e);
-			}
+				draw_tile(&l, e, c, map, 1);
 			if (map[c.y + 1][c.x])
-			{
-				margin_adjustment(&l, c, 2);
-				z_adjustment(&l, c, map, 2);
-				draw_tile(&l, e);
-			}
+				draw_tile(&l, e, c, map, 2);
 			c.x++;
 		}
 		if (map[c.y + 1][c.x])
-		{
-			margin_adjustment(&l, c, 2);
-			z_adjustment(&l, c, map, 2);
-			draw_tile(&l, e);
-		}
+			draw_tile(&l, e, c, map, 2);
 		c.y++;
 	}
 	c.x = 0;
 	while (map[c.y][c.x])
 	{
 		if (map[c.y][c.x + 1])
-		{
-			margin_adjustment(&l, c, 1);
-			z_adjustment(&l, c, map, 1);
-			draw_tile(&l, e);
-		}
+			draw_tile(&l, e, c, map, 1);
 		c.x++;
 	}
 }
 
-int		main(int ac, char **av)
+int				main(int ac, char **av)
 {
 	char	***map;
 	int		a;
@@ -94,12 +80,9 @@ int		main(int ac, char **av)
 	else
 	{
 		map = map_parser(av[1]);
-
 		e.mlx = mlx_init();
 		e.win = mlx_new_window(e.mlx, 1100, 1100, "FdF maggle");
-
 		draw_wireframe(map, e.mlx);
-
 		mlx_key_hook(e.win, (int (*)(void))my_key_funct, 0);
 		mlx_loop(e.mlx);
 	}
