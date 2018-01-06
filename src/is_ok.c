b45d9138c28fbf0c2c60f5b243982ad41eb75cea
Arthur MEHMETOGLU <amehmeto@tudent.42.fr>
2018-01-06 21:24:10 +0100

    ajout du fichier auteur

--- src/is_ok.c
+++ src/is_ok.c
@@ -6,7 +6,7 @@
 /*   By: amehmeto <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/01/05 05:20:48 by amehmeto          #+#    #+#             */
-/*   Updated: 2018/01/05 13:41:22 by amehmeto         ###   ########.fr       */
+/*   Updated: 2018/01/06 20:57:45 by amehmeto         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,21 @@ static int			counter(char **tab)
 	while (tab[i])
 		i++;
 	return (i);
+}
+
+static int			check_line_size(char *line, int size)
+{
+	char	**splitted_line;
+
+	if (!(splitted_line = ft_strsplit(line, ' ')))
+		return (9);
+	if (counter(splitted_line) != size)
+	{
+		free(splitted_line);
+		return (3);
+	}
+	free(splitted_line);
+	return (0);
 }
 
 int					is_ok(const char *av)
@@ -36,16 +51,16 @@ int					is_ok(const char *av)
 	}
 	if (get_next_line(fd, &line) == 1)
 	{
-		splitted_line = ft_strsplit(line, ' ');
+		if (!(splitted_line = ft_strsplit(line, ' ')))
+			return (9);
 		size = counter(splitted_line);
+		free(splitted_line);
 	}
 	else
 		return (2);
 	while (get_next_line(fd, &line) == 1)
-	{
-		if (counter(splitted_line = ft_strsplit(line, ' ')) != size)
+		if (check_line_size(line, size))
 			return (3);
-	}
 	if (close(fd) == -1)
 		ft_putstr("close error\n");
 	return (0);
